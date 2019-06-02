76d534980529ac9345af24ba34d5f58a02f8ece0
stump26 <moidpg@gmail.com>
2019-06-03 02:01:48 +0900

    주석작업 & issue tracking

--- round_robin.js
+++ round_robin.js
@@ -10,37 +10,40 @@ class process {
 function* round_robin(process_info_object,quantum){
   let jobTable=[];
   quantum = Number(quantum);
-  process_info_object.forEach(element => {
+  process_info_object.forEach(element => { 
     jobTable.push(new process(element));
   });
   let total_extra =0;
-  jobTable.map((e)=>{total_extra+=e.CPUcycle})
+  jobTable.map((e)=>{total_extra+=e.CPUcycle}) //전체 작업 총량 계산
   let time =0,i=0;
   let arrived_job=[];
   let rr_result=[];
-  while(total_extra > 0){
-    const i_module =i%jobTable.length;
-    if(jobTable[i_module].CPUcycle===0 ||jobTable[i_module].arrivalTime>time){
-      
-    }else if(jobTable[i_module].CPUcycle > quantum){
-      if(!arrived_job.includes(jobTable[i_module].jobName)){
+  while(total_extra > 0){  // 전체 작업을 완료했을경우 루프 탈출
+    const i_module =i%jobTable.length; 
+    if(jobTable[i_module].CPUcycle===0 ||jobTable[i_module].arrivalTime>time){ 
+      //해당순번의 작업이 완료되었거나 도착하지않은 작업일경우 넘어감
+    }else if(jobTable[i_module].CPUcycle > quantum){ 
+      if(!arrived_job.includes(jobTable[i_module].jobName)){ 
+        //처음 도착한 작업을 도착한작업배열에 확인후 없으면 넣어줌
         arrived_job.push(jobTable[i_module].jobName);
       }
-      yield {job:jobTable[i_module].jobName,from:time,to:time+quantum}
-      time+=quantum;
-      jobTable[i_module].CPUcycle-=quantum;
+      yield {job:jobTable[i_module].jobName,from:time,to:time+quantum} 
+      time+=quantum; 
+      jobTable[i_module].CPUcycle-=quantum; 
       total_extra -=quantum;
-    }else if(jobTable[i_module].CPUcycle <= quantum){
+    }else if(jobTable[i_module].CPUcycle <= quantum){ //퀀텀이하의 작업이 남은경우
       if(!arrived_job.includes(jobTable[i_module].jobName)){
-        arrived_job.push(jobTable[i_module].jobName);
+        //처음 도착한 작업을 도착한작업배열에 확인후 없으면 넣어줌
+        arrived_job.push(jobTable[i_module].jobName); 
       }
       yield {job:jobTable[i_module].jobName,from:time,to:time+jobTable[i_module].CPUcycle}
-      time+=jobTable[i_module].CPUcycle;
+      time+=jobTable[i_module].CPUcycle; //남은 작업만큼 time증가
       total_extra -= jobTable[i_module].CPUcycle;
       jobTable[i_module].CPUcycle=0;
+      arrived_job.remove(jobTable[i_module].jobName);
       rr_result.push({job:jobTable[i_module].jobName,Turnaround:time-jobTable[i_module].arrivalTime});
     }
-    if(arrived_job.length===0){
+    if(arrived_job.length===0){ //도착한작업이 없는 경우 시간 흐름.
       time++;
     }
     i++;
@@ -50,14 +53,14 @@ function* round_robin(process_info_object,quantum){
 }
 
 function result_draw(rr,interval_handle){
-  job_block =rr.next();
-  if(job_block.done){
-    clearInterval(interval_handle);
+  job_block =rr.next(); //generator함수 next(), 함수가 호출될때마다 다음yield값을 반환해준다.
+  if(job_block.done){ //generator가 작업을 완료하여return 이 반환될경우 done은 true이다.
+    clearInterval(interval_handle);//인터벌 핸들러 제거.(반복 정지.)
     const result_job_table = document.getElementById("result_job_table");
     const result_Turnaround_table = document.getElementById("result_Turnaround_table");
     const avg_turnaround = document.getElementById("avg_turnaround");
     let sum_turnaround = 0;
-    job_block.value.map(v=>{
+    job_block.value.map(v=>{ //각 작업마다 turnaround time을 그려주고 turnaround 총합 계산
       console.log(v);
       result_job_table.innerHTML += `<td>${v.job}</td>`
       result_Turnaround_table.innerHTML += `<td>${v.Turnaround}</td>`
